e770877492ef5ccac5d35f87452207ebc26d4881
Tomas <stomass2015@gmail.com>
2017-06-30 19:37:05 +0300

    Edit qt frame

--- src/chainparamsseeds.h
+++ src/chainparamsseeds.h
@@ -3,18 +3,18 @@
 
 static const unsigned int pnSeed[] =
 {
-    0x05fab2da,
-    0x05fab2db,
-    0x68a766cc,
-    0x68a766ce,
+//    0x05fab2da,
+//    0x05fab2db,
+//    0x68a766cc,
+//    0x68a766ce,
 };
 
 static const unsigned int pnTestnetSeed[] =
 {
-    0x05fab2da,
-    0x05fab2db,
-    0x68a766cc,
-    0x68a766ce,
+//    0x05fab2da,
+//    0x05fab2db,
+//    0x68a766cc,
+//    0x68a766ce,
 };
 
 #endif
